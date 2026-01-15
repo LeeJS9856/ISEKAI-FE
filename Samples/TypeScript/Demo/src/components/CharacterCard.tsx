c335b68a074a78602027cfddca6da812449519a1
LeeJS9856 <wnstj1057@naver.com>
2026-01-15 23:06:58 +0900

    🚑fix : description 3줄로 조정하여 이미지 공간 확보

--- Samples/TypeScript/Demo/src/components/CharacterCard.tsx
+++ Samples/TypeScript/Demo/src/components/CharacterCard.tsx
@@ -106,4 +106,10 @@ const CardDescription = styled.p`
   color: ${COLORS.card.describe};
   line-height: 1.5;
   opacity: 0.8;
+
+  display: -webkit-box;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  text-overflow: ellipsis;
 `;
