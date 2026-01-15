4055086a624c88efac4f4b4c868c6a872001ab44
LeeJS9856 <wnstj1057@naver.com>
2026-01-16 00:45:49 +0900

    🚑 fix : api 인터셉터로 토큰 갱신하도록 변경

--- Samples/TypeScript/Demo/src/api/client.ts
+++ Samples/TypeScript/Demo/src/api/client.ts
@@ -2,10 +2,22 @@ import axios from 'axios';
 import { getAccessToken } from '@/utils/kakaoAuth';
 
 const API_BASE_URL = import.meta.env.VITE_API_URL || '';
-const AUTH_TOKEN = getAccessToken();
+
 export const axiosClient = axios.create({
   baseURL: API_BASE_URL,
-  headers: {
-    Authorization: `Bearer ${AUTH_TOKEN}`
-  }
 });
+
+//요청보낼때 request 인터셉트해서 토큰 갱신하는거임
+axiosClient.interceptors.request.use(
+  (config) => {
+    const token = getAccessToken();
+    
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
